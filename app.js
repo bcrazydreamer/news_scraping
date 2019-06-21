031decc6c61fa076db6238d6be6b27ee6a9dbe36
bcrazydreamer <31788510+bcrazydreamer@users.noreply.github.com>
2019-06-21 16:45:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ app.use('/admin', admin);
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
+  console.log(err,"---1");
   next(err);
 });
 
@@ -64,7 +65,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  console.log(err,"---2");
   // render the error page
   res.status(err.status || 500);
   res.render('Error');
